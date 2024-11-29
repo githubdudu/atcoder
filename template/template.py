78cfa145d7e8a4040a87fb68a4cceeae1fb81f63
hdon694 <hdon694@aucklanduni.ac.nz>
2024-11-29 23:31:57 +1300

    init debug setting and template file

---
+++ template/template.py
@@ -0,0 +1,10 @@
+N, M = [int(x) for x in input().split()]
+An = [int(x) for x in input().split()]
+a = int(input())
+b, c = map(int, input().split())
+s = input()
+
+ans = 0
+print(ans)
+from bisect import bisect_left, bisect_right
+from itertools import permutations
