43cbda99970b566b2413a10f6deb7f322d28e1cb
hdon694 <hdon694@aucklanduni.ac.nz>
2024-12-18 12:00:34 +1300

    added compile and debug config (similiar to Atcoder judger)

---
+++ template/template.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+#include "atcoder/dsu.hpp"
+using namespace std;
+using namespace atcoder;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    int N;
+    cin >> N;
+
+    cout << N << endl;
+}
